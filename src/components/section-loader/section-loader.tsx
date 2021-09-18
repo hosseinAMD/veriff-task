269fa3cc0ee020938089f9517abcf443383e790e
hosseinAMD <hossein.ahmadi98@outlook.com>
2021-09-18 19:09:55 +0430

    feat: SectionLoader implemented

---
+++ src/components/section-loader/section-loader.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import t from 'i18n';
+import Button from 'components/button/button';
+import './section-loader.css';
+
+export interface SectionLoaderProps {
+  loading: boolean;
+  error?: string;
+  onTryAgain: () => void;
+  render: () => void;
+}
+
+const SectionLoader: React.FC<SectionLoaderProps> = ({
+  loading,
+  error,
+  onTryAgain,
+  render,
+}) => {
+  if (loading) {
+    return (
+      <div className="loading">
+        <p>{t('loading')}</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="error">
+        <p>{error}</p>
+        <Button onClick={onTryAgain}>{t('tryAgain')}</Button>
+      </div>
+    );
+  }
+
+  return <>{render()}</>;
+};
+
+export default SectionLoader;
